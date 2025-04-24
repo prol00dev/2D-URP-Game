2a2ce42290b6850547b1363921cfa6c484a3ec40
kn221-bpo <pro100dollar.official@gmail.com>
2025-04-24 21:20:56 +0300

    Controll

--- Assets/Scripts/Controllers/DynamicController.cs
+++ Assets/Scripts/Controllers/DynamicController.cs
@@ -7,6 +7,7 @@ public class DynamicController : MonoBehaviour
     public KeyCode moveRightKey = KeyCode.RightArrow;
     public KeyCode moveLeftKey = KeyCode.LeftArrow;
     public KeyCode moveDownKey = KeyCode.DownArrow;
+    public KeyCode jumpKey = KeyCode.Space;
 
     public float MoveForce = 0.5f;
     public float MoveUpForce = 0.2f;
@@ -14,6 +15,8 @@ public class DynamicController : MonoBehaviour
     public float brakeForce = 0.01f;
 
     protected Rigidbody2D rb;
+
+    private bool isFlying = true;
 
     void Start()
     {
@@ -44,11 +47,29 @@ public class DynamicController : MonoBehaviour
             rb.AddForce(Vector2.left * MoveForce);
             anyKeyPressed = true;
         }
-        if (!anyKeyPressed) // && onColliderTouch
+        if (!anyKeyPressed && !isFlying)
         {
             rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, Vector2.zero, brakeForce);
             rb.angularVelocity = Mathf.Lerp(rb.angularVelocity, 0f, brakeForce);
+            Debug.Log("brake = true");
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Проверяем, касается ли объект земли
+        if (collision.gameObject.tag == "Ground")
+        {
+            isFlying = false;
+        }
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Если объект перестал касаться земли
+        if (collision.gameObject.tag == "Ground")
+        {
+            isFlying = true;
+            Debug.Log("i'm fall down");
+        }
     }
 }
