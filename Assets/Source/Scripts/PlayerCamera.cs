b27deebce5d0438543826fba86bc6cfec9592cb5
kn221-bpo <pro100dollar.official@gmail.com>
2025-04-27 21:20:09 +0300

    Asset

---
+++ Assets/Source/Scripts/PlayerCamera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private float DeltaX = 2f;
+    [SerializeField] private float DeltaY = 2f;
+    [SerializeField] private float DeltaZ = 10f;
+
+    private void Awake()
+    {
+        PlayerController Controller = GetComponent<PlayerController>();
+
+        Controller.OnPlayerUpdate += UpdatePosition;
+    }
+
+    private void UpdatePosition()
+    {
+        Camera MainCamera = Camera.main;
+
+        MainCamera.transform.position = new Vector3(
+            transform.position.x + DeltaX,
+            transform.position.y + DeltaY,
+            transform.position.z - DeltaZ
+            );
+    }
+}
---
+++ Assets/Source/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Action OnPlayerUpdate;
+
+    [SerializeField] private float Speed = 0.5f;
+
+    private SpriteRenderer SpriteRenderer;
+    private bool Grounded;
+    private bool Jumping;
+
+    private float HorizontalAxis;
+    private float VerticalAxis;
+
+    private void Awake()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        var NewValue = Input.GetAxis("Horizontal");
+
+        if (NewValue != 0f)
+        {
+            HorizontalAxis = NewValue;
+
+            SpriteRenderer.flipX = HorizontalAxis < 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Grounded)
+            {
+                VerticalAxis = 1f;
+            
+                Jumping = true;
+                Grounded = false;
+            }
+        }
+
+        if (Jumping)
+        {
+            if (Grounded)
+            {
+                VerticalAxis = 0f;
+
+                Jumping = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position =
+            new Vector3(
+                transform.position.x + HorizontalAxis * Speed,
+                transform.position.y + VerticalAxis * Speed
+                );
+
+
+        OnPlayerUpdate?.Invoke();
+    }
+
+    private void OnCollisionEnter2D(Collision2D Collision)
+    {
+        if (!Collision.gameObject.CompareTag("Ground"))
+            return;
+
+        Grounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D Collision)
+    {
+        if (!Collision.gameObject.CompareTag("Ground"))
+            return;
+
+        Grounded = false;
+    }
+}
